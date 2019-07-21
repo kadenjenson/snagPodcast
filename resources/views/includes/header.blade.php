2c727334dea307d7ed74d76b28be7b63e5bbd69e
kadenjenson <sobeit99@gmail.com>
2019-07-21 00:20:19 -0600

    added logo hover effect and refactored styles

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,17 +1,21 @@
 <div class="navbar top">
     <div class="navTopRow">
-        <div class="navTopCol logo {{Request::path() === '/' ? 'active' : ''}}">
-            <div>
+        <div class="navTopCol logo">
+            <div class="logo-container {{Request::path() === '/' ? 'active' : ''}}">
                 <a href="/">
                     <img src="https://storage.buzzsprout.com/variants/U9cdKhRYM59B46nTAoSLnrHZ/8d66eb17bb7d02ca4856ab443a78f2148cafbb129f58a3c81282007c6fe24ff2" alt="SnagPodcast">
                 </a>
             </div>
         </div>
-        <div class="navTopCol white {{Request::path() === 'about' ? 'active' : ''}}">
-            <a href="/about">About</a>
+        <div class="navTopCol white">
+            <div class="link {{Request::path() === 'about' ? 'active' : ''}}">
+                <a href="/about">About</a>
+            </div>
         </div>
-        <div class="navTopCol white {{Request::path() === 'contact' ? 'active' : ''}}">
-            <a href="/contact">Contact</a>
+        <div class="navTopCol white">
+            <div class="link {{Request::path() === 'contact' ? 'active' : ''}}">
+                <a href="/contact">Contact</a>
+            </div>
         </div>
     </div>
 </div>
