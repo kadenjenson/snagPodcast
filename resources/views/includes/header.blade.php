68fbce33c0bb46f6cc7f3e09ba782b9fdd139ca7
kadenjenson <sobeit99@gmail.com>
2019-07-29 22:00:23 -0600

    updated header style and fixed NPM infinite watch bug

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,27 +3,28 @@
     <div class="navTopRow">
         <div class="navTopCol logo">
             <div class="logo-container {{Request::path() === '/' ? 'active' : ''}}">
-                <a href="/">
-                    <img src="../images/snagPodcast-logo-dark.jpg" alt="SnagPodcast">
-                </a>
+                <a href="/"><img src="../images/logos/snagPodcast-logo-dark.jpg" alt="SnagPodcast"></a>
             </div>
         </div>
-        <div class="navTopCol white link">
+        <div class="navTopCol white">
+            <div class="link-container {{Request::path() === '/' ? 'active' : ''}}">
+                <a href="/">Home</a>
+            </div>
+        </div>
+        <div class="navTopCol white">
             <div class="link-container {{Request::path() === 'about' ? 'active' : ''}}">
                 <a href="/about">About</a>
             </div>
         </div>
-        <div class="navTopCol white link">
+        <div class="navTopCol white">
             <div class="link-container {{Request::path() === 'contact' ? 'active' : ''}}">
                 <a href="/contact">Contact</a>
             </div>
         </div>
     </div>
 
-    {{-- <div class="banner">
-        <div class="text white">
-            NAVIGATING <span>THE SNAGS</span> OF LIFE ON STORY AT A TIME
-        </div>
-    </div> --}}
+    <div class="banner">
+        <div class="text white">NAVIGATING <span>THE SNAGS</span> OF LIFE, ONE STORY AT A TIME</div>
+    </div>
 
 </div>
