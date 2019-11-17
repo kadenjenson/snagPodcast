27ef45b7013c4ed57ccaaf31e6560fa926fd07d0
kadenjenson <sobeit99@gmail.com>
2019-11-17 16:45:11 -0700

    refactors and fixed nav bar styling quirks

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,22 +1,22 @@
 <div class="navbar top">
 
-    <div class="navTopRow">
-        <div class="navTopCol logo">
+    <div class="navRow">
+        <div class="navCol logo">
             <div class="logo-container {{Request::path() === '/' ? 'active' : ''}}">
                 <a href="/"><img src="../images/logos/snagPodcast-logo-dark.jpg" alt="SnagPodcast"></a>
             </div>
         </div>
-        <div class="navTopCol white">
+        <div class="navCol white">
             <div class="link-container {{Request::path() === '/' ? 'active' : ''}}">
                 <a href="/">Home</a>
             </div>
         </div>
-        <div class="navTopCol white">
+        <div class="navCol white">
             <div class="link-container {{Request::path() === 'about' ? 'active' : ''}}">
                 <a href="/about">About</a>
             </div>
         </div>
-        <div class="navTopCol white">
+        <div class="navCol white">
             <div class="link-container {{Request::path() === 'contact' ? 'active' : ''}}">
                 <a href="/contact">Contact</a>
             </div>
