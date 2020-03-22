0fca5bebc27c3a458b4500124152c1762fc926d0
kadenjenson <sobeit99@gmail.com>
2020-03-22 15:04:36 -0600

    moved over all assets and updated templates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>@yield('title', 'Snag Podcast')</title>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, intial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-    @include('includes.head')
-</head>
+        <title>@yield('title', 'Snag Podcast')</title>
+    </head>
 
-<body>
-    <div class="main-app-container">
+    <body>
+        <div class="main-app-container">
 
-        <header class="nav-wrapper">
-            @include('includes.navbar')
-        </header>
+            <header class="nav-wrapper">
+                @include('includes.navbar')
+            </header>
 
-        <div class="content-wrapper">
-            <div class="@yield('page-class', 'page')">
-                @yield('page-header', '')
-                @yield('page')
+            <div class="content-wrapper">
+                <div class="@yield('page-class', 'page')">
+                    @yield('page-header', '')
+                    @yield('page')
+                </div>
             </div>
+
+            <footer class="footer-wrapper">
+                @include('includes.footer')
+            </footer>
+
         </div>
-
-        <footer class="footer-wrapper">
-            @include('includes.footer')
-        </footer>
-
-    </div>
-</body>
+    </body>
 </html>
